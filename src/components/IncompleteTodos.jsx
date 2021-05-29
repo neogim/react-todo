509778d821b8093a8a57cbc0cc505359e29aadcb
neogim <75684023+neogim@users.noreply.github.com>
2021-05-29 18:27:52 +0900

    component化 compoinent化

--- src/components/IncompleteTodos.jsx
+++ src/components/IncompleteTodos.jsx
@@ -2,7 +2,7 @@ export const IncompleteTodos = (props) => {
   const { todos, onClickComplete, onClickDelete } = props;
   return (
     <div className="incomplete-area">
-      <p className-="title">未完了のTODO</p>
+      <p className="title">未完了のTODO</p>
       <ul>
         {todos.map((todo, index) => {
           return (
